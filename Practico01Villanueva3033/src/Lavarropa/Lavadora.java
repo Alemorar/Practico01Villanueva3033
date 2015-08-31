1dd02d183c51175b65052867bae12be792242f4d
Alemorar <infoserv100@gmail.com>
2015-08-31 10:28:15 -0300

    definimos propiedades de la clase Lavadora y agregamos constructor predefinido

--- Practico01Villanueva3033/src/Lavarropa/Lavadora.java
+++ Practico01Villanueva3033/src/Lavarropa/Lavadora.java
@@ -10,5 +10,27 @@ package Lavarropa;
  * @author alejandro
  */
 public class Lavadora {
+    private int serie;
+    private String fabricante;
+
+    public Lavadora() {
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    
     
 }
