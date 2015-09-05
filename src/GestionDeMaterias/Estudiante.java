21a966c10633ea2c836775fe1892bb834045315b
Alemorar <infoserv100@gmail.com>
2015-09-05 10:44:33 -0300

    arreglamos codigo

---
+++ src/GestionDeMaterias/Estudiante.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GestionDeMaterias;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Estudiante extends Persona{
+    private String carrera;
+    private int legajo;//se corrige tipo de dato lonint
+
+    public Estudiante() {
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+    
+    
+}
